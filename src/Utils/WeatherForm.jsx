d28d4cbe2e9c0354cb26aaac4bcdc796b8a8903f
Vitor Correa <vrcnode@gmail.com>
2023-08-04 00:40:33 -0300

    Adicionado o ingles

--- src/Utils/WeatherForm.jsx
+++ src/Utils/WeatherForm.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const WeatherForm = ({ city, handleInput, handleCityChange }) => {
-	return (
-		<form
-			onSubmit={handleInput}
-			className="d-flex justify-content-center p-4"
-			style={{ width: "350px" }}>
-			<input
-				type="text"
-				name="cityInput"
-				value={city}
-				onChange={handleCityChange}
-				placeholder="Digite o nome da cidade"
-				className="form-control"
-			/>
-			<div className="input-group-append">
-				<Button type="submit">Enviar</Button>
-			</div>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleInput}
+      className="d-flex justify-content-center p-4"
+      style={{ width: '350px' }}
+    >
+      <input
+        type="text"
+        name="cityInput"
+        value={city}
+        onChange={handleCityChange}
+        placeholder="Enter the city"
+        className="form-control"
+      />
+      <div className="input-group-append">
+        <Button type="submit">Submit</Button>
+      </div>
+    </form>
+  );
 };
 
 export default WeatherForm;
